5a4c136767df79c2d40d68bc438bf20de02920d5
falko <falko@falko-Vostro1310>
2012-03-08 12:23:10 +0100

    new files

--- hand-steuer-geraet/main.c
+++ hand-steuer-geraet/main.c
@@ -6,42 +6,87 @@
  */
 
 #include <avr/io.h>
-#ifndef F_CPU
-#warning "F_CPU war noch nicht definiert, wird nun mit 8MHz definiert"
-#define F_CPU 8000000UL     /* Quarz mit 8 Mhz */
-#endif
-#include <util/delay.h>
+#include <avr/interrupt.h>
 
-#define LED_PORT	PORTD
-#define LED_PIN		5
+uint8_t readSwitches() {
+	uint8_t switches = ((~PINC) & 0x3F);
+	switches |= ((~(PINB >> 7)) & 0x01) << 6;
+	return switches;
+}
 
-#define SWITCH_0_0_Port	PORTD
-#define SWITCH_0_0_Pin	2
-#define SWITCH_1_0_Port	PORTD
-#define SWITCH_1_1_Pin	3
-#define SWITCH_2_0_Port	PORTD
-#define SWITCH_2_1_Pin	4
+volatile uint8_t blinkCount = 6;
+uint16_t blinkInterval = 200;
+#define SWITCH_REREAD 50
+
+#define REREAD_SWITCHES 0
+volatile uint8_t flags = 0;
+
+uint16_t msClock = 0;
+uint16_t msCount = 0;
+
+ISR (TIMER0_OVF_vect) {
+	/* Interrupt Aktion alle
+	 (8000000/8)/256 Hz = 3906 Hz
+	 bzw. 0.256 ms
+	 */
+
+	// true if a millisecond has passed
+	if (msClock++ > F_CPU / 1000 / 8 / 256) {
+		msClock = 0;
+
+		// increment the milliSeconds
+		msCount++;
+
+		// reread the switches
+		if (msCount % SWITCH_REREAD == 0) {
+			flags |= 1 << REREAD_SWITCHES;
+		}
+
+		// let the LED blink
+		if (msCount % blinkInterval == 0) {
+//			if (PINC & (1 << PINC0)) {
+//				blinkCount += 1;
+//			}
+			if (blinkCount > 0) {
+				PORTB ^= 1 << DDB6;
+				blinkCount--;
+			}
+		}
+	}
+}
 
 uint8_t switches = 0;
-
-void readSwitches() {
-	switches = PINC & 0x3F;
-	switches |= ((PINB >> 7) & 0x01) << 6;
-}
 
 int main() {
 	/* Setze Eingang für Taster */
 	DDRB = 0;
 	DDRC = 0;
 
-	/* Setze Ausgang für LED an PortD */
-	DDRD = 1 << LED_PIN;
+	/* Setze Ausgang für LED und UART */
+	DDRB |= 1 << DDB6;
+	DDRD |= (1 << DDD2) | (1 << DDD1) | (1 << DDD0);
 
-	/* Setze LED an und die PULLUPS aus */
-	PORTD = 1 << LED_PIN;
+	/* Setze PULLUPS an und LED aus */
+	PORTB = (1 << DDB7);
+	PORTC = 0xFF;
+
+	// Timer 0 konfigurieren
+	TCCR0B = (1 << CS01); // Prescaler 8
+
+	// Overflow Interrupt erlauben
+	TIMSK0 |= (1 << TOIE0);
+
+	// Global Interrupts aktivieren
+	sei();
 
 	while (1) {
-		_delay_ms(50);
-		PORTD ^= 1 << LED_PIN;
+		if (flags & (1 << REREAD_SWITCHES)) {
+			uint8_t newSwitches = readSwitches();
+			if (newSwitches != switches) {
+				switches = newSwitches;
+				blinkCount += switches * 2;
+			}
+			flags &= ~(1 << REREAD_SWITCHES);
+		}
 	}
 }
